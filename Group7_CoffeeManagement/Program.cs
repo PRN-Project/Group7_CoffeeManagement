2d90f52c8e104258aa8ad18178143807a4f31703
Tang Tan Tai <tangtaidvlp@gmail.com>
2021-10-19 19:22:46 +0700

    Add Basic UI

--- Group7_CoffeeManagement/Program.cs
+++ Group7_CoffeeManagement/Program.cs
@@ -17,7 +17,7 @@ namespace Group7_CoffeeManagement
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmCoffeeManagement());
         }
     }
 }
---
+++ Group7_CoffeeManagement/frmCoffeeManagement.Designer.cs
@@ -0,0 +1,344 @@
+﻿
+namespace Group7_CoffeeManagement
+{
+    partial class frmCoffeeManagement
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.edtDrinkPrice = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.edtDrinkID = new System.Windows.Forms.TextBox();
+            this.btnDeleteDrink = new System.Windows.Forms.Button();
+            this.btnUpdateOrder = new System.Windows.Forms.Button();
+            this.nudDrinkQuantity = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.edtDrinkName = new System.Windows.Forms.TextBox();
+            this.btnCheckOut = new System.Windows.Forms.Button();
+            this.nudAddQuantity = new System.Windows.Forms.NumericUpDown();
+            this.lvOrder = new System.Windows.Forms.ListView();
+            this.btnAddDrink = new System.Windows.Forms.Button();
+            this.cbbDrinkName = new System.Windows.Forms.ComboBox();
+            this.cbbDrinkType = new System.Windows.Forms.ComboBox();
+            this.txtTotalPrice = new System.Windows.Forms.Label();
+            this.labelTotalPrice = new System.Windows.Forms.Label();
+            this.panelTables = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDrinkQuantity)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAddQuantity)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Controls.Add(this.button5);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Controls.Add(this.btnCheckOut);
+            this.panel2.Controls.Add(this.nudAddQuantity);
+            this.panel2.Controls.Add(this.lvOrder);
+            this.panel2.Controls.Add(this.btnAddDrink);
+            this.panel2.Controls.Add(this.cbbDrinkName);
+            this.panel2.Controls.Add(this.cbbDrinkType);
+            this.panel2.Location = new System.Drawing.Point(369, 13);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(652, 429);
+            this.panel2.TabIndex = 2;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(452, 377);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(184, 52);
+            this.button5.TabIndex = 22;
+            this.button5.Text = "Chuyển bàn";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Controls.Add(this.edtDrinkPrice);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.edtDrinkID);
+            this.panel3.Controls.Add(this.btnDeleteDrink);
+            this.panel3.Controls.Add(this.btnUpdateOrder);
+            this.panel3.Controls.Add(this.nudDrinkQuantity);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.edtDrinkName);
+            this.panel3.Location = new System.Drawing.Point(452, 58);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 310);
+            this.panel3.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(0, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(24, 15);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "Giá";
+            // 
+            // edtDrinkPrice
+            // 
+            this.edtDrinkPrice.Location = new System.Drawing.Point(3, 130);
+            this.edtDrinkPrice.Name = "edtDrinkPrice";
+            this.edtDrinkPrice.Size = new System.Drawing.Size(181, 23);
+            this.edtDrinkPrice.TabIndex = 25;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 15);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "ID";
+            // 
+            // edtDrinkID
+            // 
+            this.edtDrinkID.Location = new System.Drawing.Point(3, 18);
+            this.edtDrinkID.Name = "edtDrinkID";
+            this.edtDrinkID.Size = new System.Drawing.Size(181, 23);
+            this.edtDrinkID.TabIndex = 23;
+            // 
+            // btnDeleteDrink
+            // 
+            this.btnDeleteDrink.Location = new System.Drawing.Point(3, 240);
+            this.btnDeleteDrink.Name = "btnDeleteDrink";
+            this.btnDeleteDrink.Size = new System.Drawing.Size(54, 42);
+            this.btnDeleteDrink.TabIndex = 21;
+            this.btnDeleteDrink.Text = "Xóa";
+            this.btnDeleteDrink.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdateOrder
+            // 
+            this.btnUpdateOrder.Location = new System.Drawing.Point(64, 240);
+            this.btnUpdateOrder.Name = "btnUpdateOrder";
+            this.btnUpdateOrder.Size = new System.Drawing.Size(120, 43);
+            this.btnUpdateOrder.TabIndex = 20;
+            this.btnUpdateOrder.Text = "Cập nhật";
+            this.btnUpdateOrder.UseVisualStyleBackColor = true;
+            // 
+            // nudDrinkQuantity
+            // 
+            this.nudDrinkQuantity.Location = new System.Drawing.Point(4, 187);
+            this.nudDrinkQuantity.Name = "nudDrinkQuantity";
+            this.nudDrinkQuantity.Size = new System.Drawing.Size(180, 23);
+            this.nudDrinkQuantity.TabIndex = 19;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(-1, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 15);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Số lượng";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 15);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Tên ";
+            // 
+            // edtDrinkName
+            // 
+            this.edtDrinkName.Location = new System.Drawing.Point(3, 75);
+            this.edtDrinkName.Name = "edtDrinkName";
+            this.edtDrinkName.Size = new System.Drawing.Size(181, 23);
+            this.edtDrinkName.TabIndex = 17;
+            // 
+            // btnCheckOut
+            // 
+            this.btnCheckOut.Location = new System.Drawing.Point(351, 377);
+            this.btnCheckOut.Name = "btnCheckOut";
+            this.btnCheckOut.Size = new System.Drawing.Size(92, 52);
+            this.btnCheckOut.TabIndex = 8;
+            this.btnCheckOut.Text = "Thanh toán";
+            this.btnCheckOut.UseVisualStyleBackColor = true;
+            // 
+            // nudAddQuantity
+            // 
+            this.nudAddQuantity.Location = new System.Drawing.Point(267, 28);
+            this.nudAddQuantity.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.nudAddQuantity.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudAddQuantity.Name = "nudAddQuantity";
+            this.nudAddQuantity.Size = new System.Drawing.Size(95, 23);
+            this.nudAddQuantity.TabIndex = 7;
+            this.nudAddQuantity.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // lvOrder
+            // 
+            this.lvOrder.HideSelection = false;
+            this.lvOrder.Location = new System.Drawing.Point(0, 58);
+            this.lvOrder.Name = "lvOrder";
+            this.lvOrder.Size = new System.Drawing.Size(443, 310);
+            this.lvOrder.TabIndex = 4;
+            this.lvOrder.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnAddDrink
+            // 
+            this.btnAddDrink.Location = new System.Drawing.Point(368, -1);
+            this.btnAddDrink.Name = "btnAddDrink";
+            this.btnAddDrink.Size = new System.Drawing.Size(75, 52);
+            this.btnAddDrink.TabIndex = 3;
+            this.btnAddDrink.Text = "Thêm";
+            this.btnAddDrink.UseVisualStyleBackColor = true;
+            // 
+            // cbbDrinkName
+            // 
+            this.cbbDrinkName.FormattingEnabled = true;
+            this.cbbDrinkName.Location = new System.Drawing.Point(0, 29);
+            this.cbbDrinkName.Name = "cbbDrinkName";
+            this.cbbDrinkName.Size = new System.Drawing.Size(260, 23);
+            this.cbbDrinkName.TabIndex = 2;
+            // 
+            // cbbDrinkType
+            // 
+            this.cbbDrinkType.FormattingEnabled = true;
+            this.cbbDrinkType.Location = new System.Drawing.Point(0, 0);
+            this.cbbDrinkType.Name = "cbbDrinkType";
+            this.cbbDrinkType.Size = new System.Drawing.Size(362, 23);
+            this.cbbDrinkType.TabIndex = 1;
+            // 
+            // txtTotalPrice
+            // 
+            this.txtTotalPrice.AutoSize = true;
+            this.txtTotalPrice.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtTotalPrice.Location = new System.Drawing.Point(84, 10);
+            this.txtTotalPrice.Name = "txtTotalPrice";
+            this.txtTotalPrice.Size = new System.Drawing.Size(88, 21);
+            this.txtTotalPrice.TabIndex = 6;
+            this.txtTotalPrice.Text = "50.000 vnd";
+            // 
+            // labelTotalPrice
+            // 
+            this.labelTotalPrice.AutoSize = true;
+            this.labelTotalPrice.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelTotalPrice.Location = new System.Drawing.Point(3, 10);
+            this.labelTotalPrice.Name = "labelTotalPrice";
+            this.labelTotalPrice.Size = new System.Drawing.Size(75, 21);
+            this.labelTotalPrice.TabIndex = 5;
+            this.labelTotalPrice.Text = "Tổng tiền";
+            // 
+            // panelTables
+            // 
+            this.panelTables.Location = new System.Drawing.Point(12, 11);
+            this.panelTables.Name = "panelTables";
+            this.panelTables.Size = new System.Drawing.Size(351, 431);
+            this.panelTables.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.labelTotalPrice);
+            this.panel1.Controls.Add(this.txtTotalPrice);
+            this.panel1.Location = new System.Drawing.Point(369, 389);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(345, 52);
+            this.panel1.TabIndex = 9;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel4.Location = new System.Drawing.Point(455, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(181, 52);
+            this.panel4.TabIndex = 23;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1014, 449);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panelTables);
+            this.Controls.Add(this.panel2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDrinkQuantity)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAddQuantity)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.NumericUpDown nudAddQuantity;
+        private System.Windows.Forms.Label txtTotalPrice;
+        private System.Windows.Forms.Label labelTotalPrice;
+        private System.Windows.Forms.ListView lvOrder;
+        private System.Windows.Forms.Button btnAddDrink;
+        private System.Windows.Forms.ComboBox cbbDrinkName;
+        private System.Windows.Forms.ComboBox cbbDrinkType;
+        private System.Windows.Forms.FlowLayoutPanel tablePanel;
+        private System.Windows.Forms.FlowLayoutPanel panelTables;
+        private System.Windows.Forms.Button btnCheckOut;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox edtDrinkPrice;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox edtDrinkID;
+        private System.Windows.Forms.Button btnDeleteDrink;
+        private System.Windows.Forms.Button btnUpdateOrder;
+        private System.Windows.Forms.NumericUpDown nudDrinkQuantity;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox edtDrinkName;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel4;
+    }
+}
